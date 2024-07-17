75bf4565090b4474d365d9cd424200c074b75449
daniel-beck <beckdp01@gmail.com>
2024-07-17 13:00:44 -0400

    add sign in page

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Root from './routes/Root';
-import './index.css';
 import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
+
+import './index.css';
+
 import ErrorPage from './routes/Error-Page';
+import Root from './routes/Root';
+import SignIn from './routes/Sign-In';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
     errorElement: <ErrorPage />,
   }
 ]);
---
+++ src/routes/Sign-In.jsx
@@ -0,0 +1,21 @@
+export default function SignIn() {
+  
+  return (
+    <>
+        <h1>Odin Book</h1>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <p>Welcome to the Odin Book, the world's coolest fake social media app</p>
+        <form className='login_form'>
+
+          <h2>Welcome Back!</h2>
+          <label htmlFor="user_name">Username:</label>
+          <input type="text" name="user_name" id="user_name" />
+  
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" id="password" />
+  
+          <input type="submit" value="Log In" />
+        </form>
+    </>
+  );
+}
